c68db21fd15990c2178229770a8d355e0cbd6658
mplain1918 <markusplainer@hotmail.com>
2022-07-20 14:36:12 +0200

    get body parameter from form

--- src/functions/upload.js
+++ src/functions/upload.js
@@ -4,6 +4,9 @@ var slugify = require('slugify')
 const { exec } = require('child_process');
 
 exports.handler = async function (event, context) {
+  const body = JSON.parse(event.body);
+  console.log("🚀 ~ file: upload.js ~ line 8 ~ body", body)
+  
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "Hello World" }),
